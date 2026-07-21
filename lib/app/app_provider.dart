8b37713616b6347539e38f3dfe0889613b819f55
iso-abdo <isoemo79@gmail.com>
2026-07-21 20:42:58 +0300

    create: (_) => HomeProvider

--- lib/app/app_provider.dart
+++ lib/app/app_provider.dart
@@ -1,10 +1,16 @@
 import 'package:provider/provider.dart';
 
 import 'package:ludo_rank/core/dependency_injection/injection_container.dart';
+
+import 'package:ludo_rank/features/home/presentation/providers/home_provider.dart';
 import 'package:ludo_rank/features/players/presentation/providers/player_provider.dart';
 
 class AppProviders {
   static final providers = [
+    ChangeNotifierProvider<HomeProvider>(
+      create: (_) => HomeProvider(),
+    ),
+
     ChangeNotifierProvider<PlayerProvider>(
       create: (_) => sl<PlayerProvider>(),
     ),
--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -4,12 +4,18 @@ import 'package:go_router/go_router.dart';
 
 import 'package:ludo_rank/features/players/presentation/pages/players_page.dart';
 
+import '../features/home/presentation/pages/home_page.dart';
 import 'app_routes.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: AppRoutes.players,
+  initialLocation: AppRoutes.home,
 
   routes: [
+    GoRoute(
+      path: AppRoutes.home,
+      name: 'home',
+      builder: (context, state) => const HomePage(),
+    ),
 
     GoRoute(
       path: AppRoutes.players,
