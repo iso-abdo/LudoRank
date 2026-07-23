6475e0ee04ff2bb73608ac1439e78027272fe5de
iso-abdo <isoemo79@gmail.com>
2026-07-23 21:58:52 +0300

    create matches_table.dart

---
+++ lib/core/database/tables/matches_table.dart
@@ -0,0 +1,20 @@
+import 'package:drift/drift.dart';
+
+class Matches extends Table {
+  TextColumn get id => text()();
+
+  TextColumn get tournamentId => text()();
+
+  IntColumn get round => integer()();
+
+  TextColumn get status => text()();
+
+  TextColumn get winnerId => text().nullable()();
+
+  DateTimeColumn get createdAt => dateTime()();
+
+  DateTimeColumn get updatedAt => dateTime()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
