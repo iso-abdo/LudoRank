2b8bfddeb9468a8c549910179e5ca97cf98e3f01
iso-abdo <isoemo79@gmail.com>
2026-07-23 07:09:33 +0300

    create tournament_player_repository_impl.dart

---
+++ lib/features/tournament_players/data/repositories/tournament_player_repository_impl.dart
@@ -0,0 +1,53 @@
+import 'package:ludo_rank/features/tournament_players/data/datasources/local/tournament_player_dao.dart';
+import 'package:ludo_rank/features/tournament_players/data/models/tournament_player_model.dart';
+import 'package:ludo_rank/features/tournament_players/domain/entities/tournament_player.dart';
+import 'package:ludo_rank/features/tournament_players/domain/repositories/tournament_player_repository.dart';
+
+class TournamentPlayerRepositoryImpl
+    implements TournamentPlayerRepository {
+  final TournamentPlayerDao dao;
+
+  TournamentPlayerRepositoryImpl(this.dao);
+
+  @override
+  Future<List<TournamentPlayer>> getTournamentPlayers(
+      String tournamentId,
+      ) async {
+    final rows = await dao.getTournamentPlayers(
+      tournamentId,
+    );
+
+    return rows
+        .map(
+      TournamentPlayerModel.fromDrift,
+    )
+        .toList();
+  }
+
+  @override
+  Future<void> addPlayer(
+      TournamentPlayer player,
+      ) {
+    return dao.insertPlayer(
+      TournamentPlayerModel
+          .fromEntity(player)
+          .toCompanion(),
+    );
+  }
+
+  @override
+  Future<void> removePlayer(
+      String id,
+      ) {
+    return dao.removePlayer(id);
+  }
+
+  @override
+  Future<void> removeTournamentPlayers(
+      String tournamentId,
+      ) {
+    return dao.removeTournamentPlayers(
+      tournamentId,
+    );
+  }
+}
