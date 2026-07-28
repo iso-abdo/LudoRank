2a37b54675a43b8c5426037def11ff91859343a2
iso-abdo <isoemo79@gmail.com>
2026-07-28 22:00:41 +0300

    add Foreign Keys key

--- lib/core/database/tables/match_players_table.dart
+++ lib/core/database/tables/match_players_table.dart
@@ -1,11 +1,13 @@
 import 'package:drift/drift.dart';
+import 'package:ludo_rank/core/database/database.dart';
+import 'package:ludo_rank/core/database/tables/matches_table.dart';
 
 class MatchPlayers extends Table {
   TextColumn get id => text()();
 
-  TextColumn get matchId => text()();
+  TextColumn get matchId => text().references(Matches, #id)();
 
-  TextColumn get playerId => text()();
+  TextColumn get playerId => text().references(Player, #id)();
 
   /// ترتيب اللاعب داخل المباراة
   IntColumn get seat => integer()();
--- lib/core/database/tables/tournament_players_table.dart
+++ lib/core/database/tables/tournament_players_table.dart
@@ -1,11 +1,14 @@
 import 'package:drift/drift.dart';
+import 'package:ludo_rank/core/database/tables/players_table.dart';
+import 'package:ludo_rank/core/database/tables/tournaments_table.dart';
 
 class TournamentPlayers extends Table {
   TextColumn get id => text()();
 
-  TextColumn get tournamentId => text()();
+  TextColumn get tournamentId =>
+      text().references(Tournaments, #id)();
 
-  TextColumn get playerId => text()();
+  TextColumn get playerId => text().references(Players, #id)();
 
   DateTimeColumn get joinedAt =>
       dateTime().withDefault(currentDateAndTime)();
